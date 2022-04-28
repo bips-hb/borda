800afcfea7cfd472090c8f51f2e0f63255507559
Louis Dijkstra <louisdijkstra (at) gmail.com>
2022-04-28 13:50:10 +0200

    Cleaned up tabs

--- R/tabBorda.R
+++ R/tabBorda.R
@@ -3,19 +3,9 @@ tabBorda <-  tabPanel("Borda Ranking",
                      shinyjs::useShinyjs(),
                      sidebarLayout(
                        sidebarPanel(
-                         #uiOutput("selection_methods"),
-                         # 
-                         # p("Click Process to create the Bordacount with your chosen methods"),
-                         # actionButton("rnks", "Process"),
-                         # p(""),
-                         # 
-                         #p(""),
-                         #actionButton("init", "Download", icon = icon("download")),
                          downloadButton("downloadBorda", "Download")
                        ),
                        mainPanel(
                          dataTableOutput("borda_rank")
-                         #tableOutput("ranks")
-                         
                        ))
 )
--- R/tabData.R
+++ R/tabData.R
@@ -3,9 +3,8 @@ tabData <- tabPanel("Data",
                 useShinyalert(),
                        
                 sidebarLayout(
-                  # sidebar --- 
+
                   sidebarPanel(
-                    
                     # load the original data from the paper 
                     p(strong("Results from the paper")),
                     actionButton("load_original_data", "(Re)load data from the paper"),
@@ -16,21 +15,13 @@ tabData <- tabPanel("Data",
                              accept = c("text/csv",
                                        "text/comma-separated-values,text/plain",
                                        ".csv")),
-                    #actionButton("load_custom_data", "Upload"),
                     p(br()), 
-                    
                     p(
                       strong("How to upload your own ranking"),
                     )
                 ),
                            
                 mainPanel(
-                  # p("Here you see an example of the data we used in our studies"),
-                  # p("You can use this data to verify our results, but you may also use your own data"),
-                  # p("If you want to do so, please put your data in a smilar form we use and use a .csv file"),
-                  # p("It is important that the names of your csv contains 'methodname' and additional info as
-                  #            'methodname.info', exactly like the example data suggests"),
-                  # 
                   dataTableOutput("ranking")
                 ))
 )
